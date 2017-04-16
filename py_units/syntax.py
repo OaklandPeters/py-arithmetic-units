5ea3911491d91b1cba2ddc05e416cd8f44368d0f
Oakland Peters <oakland.peters@gmail.com>
2017-04-16 17:56:42 -0400

    Type hints in syntax.py

--- py_units/syntax.py
+++ py_units/syntax.py
@@ -18,6 +18,7 @@ from .base import UnitMeta
 #   unions, or interfaces as well (such as numbers.Number).
 Domain = TypeVar('Domain')
 Codomain = TypeVar('Codomain')
+EitherDomain = Union[Domain, Codomain]
 # These two function signatures should be variant on number of arguments. IE
 #       DomainFunction = Callable[Tuple[Domain, ...], Domain]
 # But Python 3.5 cannot handle this
@@ -110,7 +111,7 @@ class InvariantFunctor(Functor, Generic[Domain, Codomain], metaclass=ABCMeta):
         )
 
     def apply(self, func: DomainFunction,
-              *values: Tuple[Union[Domain, Codomain], ...]) -> Codomain:
+              *values: Tuple[EitherDomain, ...]) -> Codomain:
         """
         The .map function, but dispatches on category, so elements of codomain are
         destructed into the domain before mapping.
@@ -133,7 +134,7 @@ class InvariantFunctor(Functor, Generic[Domain, Codomain], metaclass=ABCMeta):
         return self.construct(func(*_domains))
 
 
-class ArithmeticSyntaxMixin(metaclass=UnitMeta):
+class ArithmeticSyntaxMixin(Generic[Domain, Codomain], metaclass=UnitMeta):
     """Provides arithmetic operators for a class.
 
     The 'functor' property should be over-ridden to provide
@@ -143,46 +144,46 @@ class ArithmeticSyntaxMixin(metaclass=UnitMeta):
     Abstracts:
         functor
     """
-    functor: InvariantFunctor
+    functor: InvariantFunctor[Domain, Codomain]
 
-    def __add__(self, a):
+    def __add__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__add__, self, a)
 
-    def __radd__(self, a):
+    def __radd__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__add__, a, self)
 
-    def __sub__(self, a):
+    def __sub__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__sub__, self, a)
 
-    def __rsub__(self, a):
+    def __rsub__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__sub__, a, self)
 
-    def __mul__(self, a):
+    def __mul__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__mul__, self, a)
 
-    def __rmul__(self, a):
+    def __rmul__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__mul__, a, self)
 
-    def __truediv__(self, a):
+    def __truediv__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__truediv__, self, a)
 
-    def __rtruediv__(self, a):
+    def __rtruediv__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__truediv__, a, self)
 
-    def __floordiv__(self, a):
+    def __floordiv__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__floordiv__, self, a)
 
-    def __rfloordiv__(self, a):
+    def __rfloordiv__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__floordiv__, a, self)
 
-    def __mod__(self, a):
+    def __mod__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__mod__, self, a)
 
-    def __rmod__(self, a):
+    def __rmod__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__mod__, a, self)
 
-    def __pow__(self, a):
+    def __pow__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__pow__, self, a)
 
-    def __rpow__(self, a):
+    def __rpow__(self, a: EitherDomain) -> Codomain:
         return self.functor.apply(operator.__pow__, a, self)
