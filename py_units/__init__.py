4e0239b60aaa487b158d7d0967b661d6314857d6
Oakland Peters <oakland.peters@gmail.com>
2017-04-16 17:55:50 -0400

    Add package-level __init__ with imports

---
+++ py_units/__init__.py
@@ -0,0 +1,28 @@
+from .base import UnitsError, UnitsTypeError, FunctorError, UnitsType, UnitMeta
+from .syntax import Functor, InvariantFunctor, ArithmeticSyntaxMixin
+from .units import (
+    Unit, UnitsLeaf, UnitsStem, UnitVector,
+    NumberScalarInvariantFunctor, Scalar,
+    UnitsFunction, Multiply, Divide, UnitsFunctionStem
+)
+
+__all__ = (
+    UnitsError,
+    UnitsTypeError,
+    FunctorError,
+    UnitsType,
+    UnitMeta,
+    Functor,
+    InvariantFunctor,
+    ArithmeticSyntaxMixin,
+    Unit,
+    UnitsLeaf,
+    UnitsStem,
+    UnitVector,
+    NumberScalarInvariantFunctor,
+    Scalar,
+    UnitsFunction,
+    Multiply,
+    Divide,
+    UnitsFunctionStem
+)
