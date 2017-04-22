a135461b95be9571a8dba038dedb8f20072895b6
Oakland Peters <oakland.peters@gmail.com>
2017-04-22 17:33:33 -0400

    Add dimension.py parts to package

--- unit_tree/__init__.py
+++ unit_tree/__init__.py
@@ -2,9 +2,11 @@ from .base import (
     UnitsError, UnitsTypeError, FunctorError, OperatorLookupError,
     UnitsType, UnitMeta, NotPassed, identity)
 from .tree import (Tree, Empty, Leaf, Node, bfs, dfs)
+from .dimension import (Dimension, NullUnit)
 
 __all__ = (
     Tree, Empty, Leaf, Node, bfs, dfs,
     UnitsError, UnitsTypeError, FunctorError, OperatorLookupError,
-    UnitsType, UnitMeta, NotPassed, identity
+    UnitsType, UnitMeta, NotPassed, identity,
+    Dimension, NullUnit
 )
